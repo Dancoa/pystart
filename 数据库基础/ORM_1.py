278da67b18d3773f2e72ac27137a5fc3ae68f601
Dancoa <aptxx_4869@163.com>
2022-01-16 16:45:57 +0800

    rui

---
+++ 数据库基础/ORM_1.py
@@ -0,0 +1,5 @@
+from re import T
+from sqlalchemy import Column, create_engine,String,Integer, engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
